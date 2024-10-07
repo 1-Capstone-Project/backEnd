b61c1491736c065c4e7e19baf791f5c55d03af28
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-10-07 23:30:57 +0900

    commit

---
+++ models/company.go
@@ -0,0 +1,13 @@
+package models
+
+type CompanyInfo struct {
+	ID                   int    `json:"id"`
+	CompanyName          string `json:"company_name"`
+	HeadquartersLocation string `json:"headquarters_location"`
+	Industry             string `json:"industry"`
+	Welfare              string `json:"welfare"`
+	RecruitmentMethod    string `json:"recruitment_method"`
+	Requirements         string `json:"requirements"`
+	Website              string `json:"website"`
+	ImageURL             string `json:"image_url"`
+}
