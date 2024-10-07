f3b0911a8b8c6aa73c261eae7c51e75ba531d034
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-10-07 23:30:49 +0900

    commit

--- models/post.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Post struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	ImgURL      string `json:"img_url"`
-}
